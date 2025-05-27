ef63997c950290d40609fda239f240a652749b9f
Talha343Nadeem <talhanadeem280302@outlook.com>
2025-05-27 17:53:43 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,100 +1,52 @@
-from imaplib import Flags
-
-dealAgain = "y"
-
-def main():
-    global dealAgain
-
-    import random
-    from art import logo
-    print(logo)
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    compCards = []
-    playerCards = []
-
-    def dealerDelt():
-        for i in range(2):
-            card =  random.choice(cards)
-            if card == 11 and card + sum(compCards) >21:
-                    card = 1
-            compCards.append(card)
-        while sum(compCards) < 17:
-            card = random.choice(cards)
-            compCards.append(card)
-
-    dealerDelt()
-
-    def playerDelt():
-        for i in range(2):
-
-            card = random.choice(cards)
-            if card == 11 and card + sum(playerCards) > 21:# condidtion for when you are delt an ace
-                card = 1
-
-            playerCards.append(card)
+import random
+from art import  logo
+print(logo)
+numberToGuess = random.randint(1,100)
+# print(numberToGuess)
+print("Welcome to Talha's Number guessing game")
+print("I have Chosen a number betweeen 1 and 100")
+modeSelect = input("Choose the Difficulty easy or hard: ")
+# if modeSelect == "hard":
 
 
-    playerDelt()
-    stopgame = False
-    def playerHit():
-        card = random.choice(cards)
-        playerCards.append(card)
-        if sum(playerCards) >21:
-            print(f"Your Final hand: {playerCards}, final score: {sum(playerCards)}")
-            print(f"Computers final hand: {compCards[0]}")
-            print("you went over you lose :(")
-        else:
-            print(f"Your Cards: {playerCards}, current score: {sum(playerCards)}")
-            print(f"Computers first Card: {compCards[0]}")
+# if modeSelect == "easy":
+def Game():
+    # guessRemaning = -1
+    if modeSelect == "hard":
+        guessRemaining = 5
 
-    def evalaute():
+    elif modeSelect == "easy":
 
-        if sum(playerCards) > sum(compCards):
-            print(f"Your Final hand: {playerCards}, final score: {sum(playerCards)}")
-            print(f"Computers final hand: {compCards}  final score {sum(compCards)}")
-            print("you have more you win")
+        guessRemaining = 10
+    # print(f"You have {guessRemaining} attempts to guess the number")
+    #
+    #
+    guess = ""
+    while guess != numberToGuess and guessRemaining != 0:
+        print(f"You have {guessRemaining} attempts to guess the number")
 
+        guess = input("Make a Guess :")
 
-        elif sum(compCards) > 21:
-            print(f"Your Final hand: {playerCards}, final score: {sum(playerCards)}")
-            print(f"Computers final hand: {compCards}  final score {sum(compCards)}")
-            print("opponent went over you win")
-        elif sum( compCards) > sum(playerCards):
-            print(f"Your Final hand: {playerCards}, final score: {sum(playerCards)}")
-            print(f"Computers final hand: {compCards}  final score {sum(compCards)}")
-            print("you lose")
-        elif sum(compCards) == sum(playerCards):
-            print(f"Your Final hand: {playerCards}, final score: {sum(playerCards)}")
-            print(f"Computers final hand: {compCards}  final score {sum(compCards)}")
-            print("its a draw")
+        try:
+            guess = int(guess)
+        except:
+            print('guess Again')
+            guessRemaining -=1
+            continue
 
 
+        if guess == numberToGuess:
+            print("great success")
+        elif guess > numberToGuess:
+            print("too high")
+            guessRemaining -= 1
+        elif guess < numberToGuess:
+
+           guessRemaining -= 1
+           print("too low")
+    if guessRemaining == 0:
+        print("you ran out of guesses lmao ;( ")
+
+Game()
 
 
-
-    print(f"Your Cards: {playerCards}, current score: {sum(playerCards)}")
-    print(f"Computers first Card: {compCards[0]}")
-    while dealAgain == 'y' and sum(playerCards) <= 21 :
-        dealAgain = input("type y to get another card, type n to pass:")
-
-        if dealAgain == "y":
-            playerHit()
-
-        elif dealAgain == "n":
-            evalaute()
-
-
-# main()
-
-def playGame():
-    play = True
-    while play:
-        play = input("play a game? of black jack: ")
-        if play == 'yes':
-            play =True
-            main()
-        else:
-            play = False
-            print("GoodBye from Casino de Talha, Have a nice day")
-
-playGame()
